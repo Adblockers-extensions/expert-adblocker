d7e4a6c2a54d4942807a145b86b18c81165c21b2
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-05-10 01:34:09 -0700

    HBO Max Adblocker Done

--- src/Hbomax/hbomax.js
+++ src/Hbomax/hbomax.js
@@ -1,18 +1,15 @@
-// console.log("HBOMAX Ad Blocker running.....");
-function HBOMAX() {
-  const FORWARD = 5.5;
-  const adLabel = document.querySelector('[data-testid="ad"]');
+const HBOMAX=()=>{
+  console.log("HBOMAX Ad Blocker running.....");
+  const FORWARD = 5;
+  const adtext=document.querySelector('.gigKKX')     
   const player = document.querySelectorAll("video")[0];
   if (player && player.buffered.length >= 1) {
-    if (adLabel && adLabel.style.visibility === "visible") {
-      console.log("AD DETECED");
+    if (adtext.textContent==="Ad") {
       console.log(console.log(`SKIPPING AD ON ${FORWARD}`));
-      observer.disconnect();
       fastForward(player);
       return;
     } else {
       backToNormal(player);
-      observer.disconnect();
     }
   }
   function fastForward(player) {
@@ -30,4 +27,5 @@ observer.observe(document, {
   subtree: true,
   attributes: true,
 });
-HBOMAX();
+HBOMAX();   
+
