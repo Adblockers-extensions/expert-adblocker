d6ffe3d7b63d092f8576a1cb2a910c28dc4fde3d
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-05-16 05:10:07 -0700

    Instagram Sponsored Ads Count Updated.

--- src/facebook/Instagram.tsx
+++ src/facebook/Instagram.tsx
@@ -1,18 +1,17 @@
+import adsCount from "../utils/AdsCount";
 import observeMutations from "../utils/Observer";
-
 //GET THE CURRENT URL AND RUN THE FUNCTION ACCODINGLY
 const CURRENT_URL = window.location.href;
 export async function InstagramFeedSponser() {
   if (!CURRENT_URL.startsWith("https://www.instagram.com")) return;
-
   const processFeedSponsor = () => {
     const spans = document.querySelectorAll("span");
     spans.forEach((span) => {
       if (span.textContent.includes("Sponsored")) {
         const parentElement = span.parentNode;
         if (parentElement && parentElement instanceof HTMLElement) {
-          parentElement.style.display = "none";
-          // observer.disconnect();
+          parentElement.remove();
+          adsCount(Promise.resolve(1))
         }
       }
     });
